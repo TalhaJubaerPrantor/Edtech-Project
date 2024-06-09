915efde26ad34dadcae46192f3edc615e0330374
Kunj05 <asuraaaaa05@gmail.com>
2024-06-09 19:02:36 +0530

    Home Page Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+        </Routes>
       
     </div>
   );
